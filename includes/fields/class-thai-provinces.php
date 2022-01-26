10c01e010fa201c2e390f9eabffbdf991bdfc9d0
ITANKAWIN <itankawin@gmail.com>
2022-01-26 22:01:14 +0700

    แก้ขึ้น Warning: Illegal string offset

--- includes/fields/class-thai-provinces.php
+++ includes/fields/class-thai-provinces.php
@@ -25,91 +25,89 @@ class Field_Provinces {
         $default_config = array(
             'id' => $_POST['field_id'],
             'type' => $this->field_type,
-            'provinces' => array(
-                'label' => 'Provinces',
-                'option_default' => 1,
-                'options' => array(
-                    1 => 'กรุงเทพมหานคร',
-                    2 => 'จังหวัดกระบี่',
-                    3 => 'จังหวัดกาญจนบุรี',
-                    4 => 'จังหวัดกาฬสินธุ์',
-                    5 => 'จังหวัดกำแพงเพชร',
-                    6 => 'จังหวัดขอนแก่น',
-                    7 => 'จังหวัดจันทบุรี',
-                    8 => 'จังหวัดฉะเชิงเทรา',
-                    9 => 'จังหวัดชลบุรี',
-                    10 => 'จังหวัดชัยนาท',
-                    11 => 'จังหวัดชัยภูมิ',
-                    12 => 'จังหวัดชุมพร',
-                    13 => 'จังหวัดเชียงราย',
-                    14 => 'จังหวัดเชียงใหม่',
-                    15 => 'จังหวัดตรัง',
-                    16 => 'จังหวัดตราด',
-                    17 => 'จังหวัดตาก',
-                    18 => 'จังหวัดนครนายก',
-                    19 => 'จังหวัดนครปฐม',
-                    20 => 'จังหวัดนครพนม',
-                    21 => 'จังหวัดนครราชสีมา',
-                    22 => 'จังหวัดนครศรีธรรมราช',
-                    23 => 'จังหวัดนครสวรรค์',
-                    24 => 'จังหวัดนนทบุรี',
-                    25 => 'จังหวัดนราธิวาส',
-                    26 => 'จังหวัดน่าน',
-                    27 => 'จังหวัดบึงกาฬ',
-                    28 => 'จังหวัดบุรีรัมย์',
-                    29 => 'จังหวัดปทุมธานี',
-                    30 => 'จังหวัดประจวบคีรีขันธ์',
-                    31 => 'จังหวัดปราจีนบุรี',
-                    32 => 'จังหวัดปัตตานี',
-                    33 => 'จังหวัดพระนครศรีอยุธยา',
-                    34 => 'จังหวัดพะเยา',
-                    35 => 'จังหวัดพังงา',
-                    36 => 'จังหวัดพัทลุง',
-                    37 => 'จังหวัดพิจิตร',
-                    38 => 'จังหวัดพิษณุโลก',
-                    39 => 'จังหวัดเพชรบุรี',
-                    40 => 'จังหวัดเพชรบูรณ์',
-                    41 => 'จังหวัดแพร่',
-                    42 => 'จังหวัดภูเก็ต',
-                    43 => 'จังหวัดมหาสารคาม',
-                    44 => 'จังหวัดมุกดาหาร',
-                    45 => 'จังหวัดแม่ฮ่องสอน',
-                    46 => 'จังหวัดยโสธร',
-                    47 => 'จังหวัดยะลา',
-                    48 => 'จังหวัดร้อยเอ็ด',
-                    49 => 'จังหวัดระนอง',
-                    50 => 'จังหวัดระยอง',
-                    51 => 'จังหวัดราชบุรี',
-                    52 => 'จังหวัดลพบุรี',
-                    53 => 'จังหวัดลำปาง',
-                    54 => 'จังหวัดลำพูน',
-                    55 => 'จังหวัดเลย',
-                    56 => 'จังหวัดศรีสะเกษ',
-                    57 => 'จังหวัดสกลนคร',
-                    58 => 'จังหวัดสงขลา',
-                    59 => 'จังหวัดสตูล',
-                    60 => 'จังหวัดสมุทรปราการ',
-                    61 => 'จังหวัดสมุทรสงคราม',
-                    62 => 'จังหวัดสมุทรสาคร',
-                    63 => 'จังหวัดสระแก้ว',
-                    64 => 'จังหวัดสระบุรี',
-                    65 => 'จังหวัดสิงห์บุรี',
-                    66 => 'จังหวัดสุโขทัย',
-                    67 => 'จังหวัดสุพรรณบุรี',
-                    68 => 'จังหวัดสุราษฎร์ธานี',
-                    69 => 'จังหวัดสุรินทร์',
-                    70 => 'จังหวัดหนองคาย',
-                    71 => 'จังหวัดหนองบัวลำภู',
-                    72 => 'จังหวัดอ่างทอง',
-                    73 => 'จังหวัดอำนาจเจริญ',
-                    74 => 'จังหวัดอุดรธานี',
-                    75 => 'จังหวัดอุตรดิตถ์',
-                    76 => 'จังหวัดอุทัยธานี',
-                    77 => 'จังหวัดอุบลราชธานี'
-                ),
-                'next_option_id' => 78,
+            'label' => 'Provinces',
+            'option_default' => 1,
+            'next_option_id' => "78",
+            #provinces
+            'options' => array(
+                1 => 'กรุงเทพมหานคร',
+                2 => 'จังหวัดกระบี่',
+                3 => 'จังหวัดกาญจนบุรี',
+                4 => 'จังหวัดกาฬสินธุ์',
+                5 => 'จังหวัดกำแพงเพชร',
+                6 => 'จังหวัดขอนแก่น',
+                7 => 'จังหวัดจันทบุรี',
+                8 => 'จังหวัดฉะเชิงเทรา',
+                9 => 'จังหวัดชลบุรี',
+                10 => 'จังหวัดชัยนาท',
+                11 => 'จังหวัดชัยภูมิ',
+                12 => 'จังหวัดชุมพร',
+                13 => 'จังหวัดเชียงราย',
+                14 => 'จังหวัดเชียงใหม่',
+                15 => 'จังหวัดตรัง',
+                16 => 'จังหวัดตราด',
+                17 => 'จังหวัดตาก',
+                18 => 'จังหวัดนครนายก',
+                19 => 'จังหวัดนครปฐม',
+                20 => 'จังหวัดนครพนม',
+                21 => 'จังหวัดนครราชสีมา',
+                22 => 'จังหวัดนครศรีธรรมราช',
+                23 => 'จังหวัดนครสวรรค์',
+                24 => 'จังหวัดนนทบุรี',
+                25 => 'จังหวัดนราธิวาส',
+                26 => 'จังหวัดน่าน',
+                27 => 'จังหวัดบึงกาฬ',
+                28 => 'จังหวัดบุรีรัมย์',
+                29 => 'จังหวัดปทุมธานี',
+                30 => 'จังหวัดประจวบคีรีขันธ์',
+                31 => 'จังหวัดปราจีนบุรี',
+                32 => 'จังหวัดปัตตานี',
+                33 => 'จังหวัดพระนครศรีอยุธยา',
+                34 => 'จังหวัดพะเยา',
+                35 => 'จังหวัดพังงา',
+                36 => 'จังหวัดพัทลุง',
+                37 => 'จังหวัดพิจิตร',
+                38 => 'จังหวัดพิษณุโลก',
+                39 => 'จังหวัดเพชรบุรี',
+                40 => 'จังหวัดเพชรบูรณ์',
+                41 => 'จังหวัดแพร่',
+                42 => 'จังหวัดภูเก็ต',
+                43 => 'จังหวัดมหาสารคาม',
+                44 => 'จังหวัดมุกดาหาร',
+                45 => 'จังหวัดแม่ฮ่องสอน',
+                46 => 'จังหวัดยโสธร',
+                47 => 'จังหวัดยะลา',
+                48 => 'จังหวัดร้อยเอ็ด',
+                49 => 'จังหวัดระนอง',
+                50 => 'จังหวัดระยอง',
+                51 => 'จังหวัดราชบุรี',
+                52 => 'จังหวัดลพบุรี',
+                53 => 'จังหวัดลำปาง',
+                54 => 'จังหวัดลำพูน',
+                55 => 'จังหวัดเลย',
+                56 => 'จังหวัดศรีสะเกษ',
+                57 => 'จังหวัดสกลนคร',
+                58 => 'จังหวัดสงขลา',
+                59 => 'จังหวัดสตูล',
+                60 => 'จังหวัดสมุทรปราการ',
+                61 => 'จังหวัดสมุทรสงคราม',
+                62 => 'จังหวัดสมุทรสาคร',
+                63 => 'จังหวัดสระแก้ว',
+                64 => 'จังหวัดสระบุรี',
+                65 => 'จังหวัดสิงห์บุรี',
+                66 => 'จังหวัดสุโขทัย',
+                67 => 'จังหวัดสุพรรณบุรี',
+                68 => 'จังหวัดสุราษฎร์ธานี',
+                69 => 'จังหวัดสุรินทร์',
+                70 => 'จังหวัดหนองคาย',
+                71 => 'จังหวัดหนองบัวลำภู',
+                72 => 'จังหวัดอ่างทอง',
+                73 => 'จังหวัดอำนาจเจริญ',
+                74 => 'จังหวัดอุดรธานี',
+                75 => 'จังหวัดอุตรดิตถ์',
+                76 => 'จังหวัดอุทัยธานี',
+                77 => 'จังหวัดอุบลราชธานี'
             )
-
         );
         $position = "<input type='hidden' name='panel[{$_POST['field_id']}]' class='panel' value=''>";
         // Prepare to return compiled results.
@@ -131,40 +129,23 @@ class Field_Provinces {
 
         $preview = "";
         $preview .= "<div class='ui form big'>";
-        $preview .= "<div class='three fields'>";
         $preview .= "<div class='field'>";
-        if (isset($config['label']['province'])) {
+        if (isset($config['label'])) {
 
-            $preview .= "<label>{$config['label']['province']}</label>";
+            $preview .= "<label id='{$config['id']}'>{$config['label']}</label>";
         }
-        $preview .= "<select name='field_id[{$config['id']}][province]' id='{$config['id']}' class='ui dropdown fluid' disabled>";
-        foreach ($config['options']['province'] as $option) {
-            $preview .= "<option value='$option' " . ($option === $config['option_default']['province'] ? 'selected' : '') . ">{$option}</option>";
+        $preview .= "<select name='field_id[{$config['id']}]' id='{$config['id']}' class='ui dropdown fluid' disabled>";
+        foreach ($config['options'] as $option) {
+            $preview .= "<option value='$option' " . ($option === $config['option_default'] ? 'selected' : '') . ">{$option}</option>";
         }
         $preview .= "</select>";
-        $preview .= "</div>";
-        $preview .= "<div class='field'>";
-        if (isset($config['label']['amphure'])) {
-
-            $preview .= "<label>{$config['label']['amphure']}</label>";
-        }
-        $preview .= "<select name='field_id[{$config['id']}][amphure]' id='{$config['id']}' class='ui dropdown fluid'>";
-        $preview .= "</select>";
-        $preview .= "</div>";
-        $preview .= "<div class='field'>";
-        if (isset($config['label']['district'])) {
-
-            $preview .= "<label>{$config['label']['district']}</label>";
-        }
-        $preview .= "<select name='field_id[{$config['id']}][district]' id='{$config['id']}' class='ui dropdown fluid'>";
-        $preview .= "</select>";
-        $preview .= "</div>";
         $preview .= "</div>";
         $preview .= "</div>";
         return $preview;
     }
 
     public function config($config = []) {
+
         $config_field = "
         <div class='wrapper-instance-pane properties-config config_field_{$config['id']}' data-field-id='{$config['id']}' style='display: none;'>
             <div class='ui two column grid'>
@@ -199,7 +180,7 @@ class Field_Provinces {
                     </div>
                     <div class='column'>
                         <div class='ui fluid input'>
-                            <input type='text' class='config-form-label' name='fields[{$config['id']}][label][province]' value='{$config['label']['province']}'>
+                            <input type='text' class='config-form-label' name='fields[{$config['id']}][label]' value='{$config['label']}'>
                         </div>
                     </div>
                 </div>
@@ -209,14 +190,14 @@ class Field_Provinces {
                     </div>
                     <div class='column'>
                         <div class='ui fluid input'>
-                            <input type='text' class='config-form-name' name='fields[{$config['id']}][name][province]' value='{$config['name']['province']}'>
+                            <input type='text' class='config-form-name' name='fields[{$config['id']}][name]' value='{$config['name']}'>
                         </div>
                     </div>
                 </div>  
             </div>
             <hr>
 
-            <input type='hidden' name='fields[{$config['id']}][next_option_id]' value='{$config['id']['next_option_id']}'>
+            <input type='hidden' name='fields[{$config['id']}][next_option_id]' value='{$config['next_option_id']}'>
             <div class='ui grid'>
                 <div class='four wide column'>
                     <div class='column'>
@@ -224,11 +205,11 @@ class Field_Provinces {
                     </div>
                     <div class='four wide column'>
                         ";
-        foreach ($config['options']['province'] as $option => $v) {
+        foreach ($config['options'] as $option => $v) {
             $config_field .= "
                         <div class='ui fluid input'>
-                            <div class='index-control'><input type='radio' name='fields[{$config['id']}][option_default][province]' " . ($config['option_default']['province'] == $v ? 'checked' : '') . " value='{$v}'></div>
-                            <input type='text' class='form-control' name='fields[{$config['id']}][options][province][{$option}]' value='{$v}'>
+                            <div class='index-control'><input type='radio' name='fields[{$config['id']}][option_default]' " . ($config['option_default'] == $v ? 'checked' : '') . " value='{$v}'></div>
+                            <input type='text' class='form-control' name='fields[{$config['id']}][options][{$option}]' value='{$v}'>
                             <a class='add' href='#'>
                                 <i class='icon plus circle green'></i>
                             </a>
