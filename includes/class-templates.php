96f6bcc25fa35886df4cea15fe76070b5fca5fb7
ITANKAWIN <itankawin@gmail.com>
2022-02-09 22:19:58 +0700

    เพิ่ม template login

--- includes/class-templates.php
+++ includes/class-templates.php
@@ -56,16 +56,90 @@ class Codex_Templates {
 
     function Template_Register() {
         $data = array(
-            'fields'    => [],
-            'panels'    => 12,
-            'panel'     => [],
-            'setting'   => [
-                'succ_msg'  => 'Thankyou for submit form.',
+            'fields' =>
+            array(
+                445087 =>
+                array(
+                    'id' => '445087',
+                    'type' => 'email_user',
+                    'label' => 'Email',
+                    'placeholder' => 'name@gmail.com',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+                982292 =>
+                array(
+                    'id' => '982292',
+                    'type' => 'submit',
+                    'text' => 'Submit',
+                    'align' => 'left',
+                ),
+                613439 =>
+                array(
+                    'id' => '613439',
+                    'type' => 'first_name',
+                    'label' => 'First Name',
+                    'placeholder' => 'first name',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+                102111 =>
+                array(
+                    'id' => '102111',
+                    'type' => 'last_name',
+                    'label' => 'Last Name',
+                    'placeholder' => 'last name',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+                355616 =>
+                array(
+                    'id' => '355616',
+                    'type' => 'username',
+                    'label' => 'Username',
+                    'placeholder' => 'username',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+                996450 =>
+                array(
+                    'id' => '996450',
+                    'type' => 'password_user',
+                    'label' => 'Password',
+                    'placeholder' => 'password',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+                846074 =>
+                array(
+                    'id' => '846074',
+                    'type' => 'con_password_user',
+                    'label' => 'Confirm Password',
+                    'placeholder' => 'confirm password',
+                    'value' => '',
+                    'require' => 'on',
+                ),
+            ),
+            'id' => '34',
+            'panels' => '6:6|12|12|12|12|12',
+            'panel' =>
+            array(
+                613439 => '1:1',
+                102111 => '1:2',
+                445087 => '2:1',
+                355616 => '3:1',
+                996450 => '4:1',
+                846074 => '5:1',
+                982292 => '6:1',
+            ),
+            'setting' =>
+            array(
+                'succ_msg' => 'Thankyou for submit form.',
                 'redirect' => '',
-                'err_msg'  => 'Something Went wrong!!',
+                'err_msg' => 'Something Went wrong!!',
                 'err_redirect' => '',
-                'template'  => 'register',
-            ],
+                'template' => 'register',
+            ),
         );
 
         return $data;
